fd979c80bcf30a2b385ac5714ba97b864f043c78
vikas <vikas80046@gmail.com>
2024-02-01 09:51:53 +0530

    changes in config file

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  server:{
-    proxy:{
-      '/api':'http://localhost:1200'
-    }
+  server: {
+    proxy: {
+      "/api": "http://localhost:1200",
+    },
   },
-=======
-  // server:{
-  //   proxy:{
-  //     '/api':'rpaic-backend.vercel.app:1200'
-  //   }
-  // },
->>>>>>> af4f9651ddea0ffe3ea012d7cc16c6847d23fc25
+
   plugins: [react()],
-})
+});
