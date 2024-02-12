41b3f611d1712256f97a6dea92825564b26084df
vikas <vikas80046@gmail.com>
2024-02-13 04:51:14 +0530

    some error fixed

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  server:{
-    proxy:{
-      '/api':'http://localhost:1200'
-    }
-  },
-=======
+
   // server:{
   //   proxy:{
-  //     '/api':'rpaic-backend.vercel.app:1200'
+  //     '/api':'http://localhost:1200'
   //   }
   // },
->>>>>>> af4f9651ddea0ffe3ea012d7cc16c6847d23fc25
+
   plugins: [react()],
 })
