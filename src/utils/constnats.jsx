db9c610b3460ec06fc197efb5e6a055a52309059
vikas <vikas80046@gmail.com>
2024-02-29 23:16:50 +0530

    made some changes

--- src/utils/constnats.jsx
+++ src/utils/constnats.jsx
@@ -1,5 +1,5 @@
-// export const baseUrl = "https://rpaic-backend.onrender.com";
+export const baseUrl = "https://rpaic-backend.onrender.com";
 
-export const baseUrl = "http://localhost:1200";
+// export const baseUrl = "http://localhost:1200";
 
 export const years = ["2023-2024", "2024-2025"];
