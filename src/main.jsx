3120f280391ed8f98c7eccec81649175dd107e72
vikas <vikas80046@gmail.com>
2024-02-29 19:47:19 +0530

    fixing some issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import {
+  createHashRouter,
+  RouterProvider,
+  
+  BrowserRouter,
+} from "react-router-dom";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: (
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    ),
-  },
-]);
+// const router = createHashRouter([
+//   {
+//     path: "/*",
+//     element: (
+//       <ChakraProvider>
+//         <App />
+//       </ChakraProvider>
+//     ),
+//   },
+// ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter>
+    
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+     
+    </BrowserRouter>
   </React.StrictMode>
 );
